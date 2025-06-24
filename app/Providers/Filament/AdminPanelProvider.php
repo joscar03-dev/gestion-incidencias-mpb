73d3c16a35094d55c5c976b32de42234610dd6f7
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-06-24 12:34:18 -0500

    sidebar collapsable

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->profile()
+            ->sidebarCollapsibleOnDesktop()
             ->colors([
                 'primary' => Color::Amber,
             ])
