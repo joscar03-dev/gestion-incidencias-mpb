23d14620a973ae8b39c82890ea51b3d19451027b
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-06-25 13:39:55 -0500

    campo creado creado_por

---
+++ database/migrations/2025_06_25_120046_agregar_campo_creador_por.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->unsignedBigInteger('creado_por')->after('id');
+            $table->foreign('creado_por')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropForeign(['creado_por']);
+            $table->dropColumn('creado_por');
+        });
+    }
+};
