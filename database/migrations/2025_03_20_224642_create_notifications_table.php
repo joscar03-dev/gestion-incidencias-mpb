813a5296e7ab88d26104313b52f2b1b2b237562e
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-06-25 13:39:43 -0500

    tabla notificaciones

---
+++ database/migrations/2025_03_20_224642_create_notifications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    /* This migration creates the notifications table.
+     * The table stores notifications for users, including the type of notification,
+     * the notifiable entity, the data associated with the notification, and a timestamp
+     * for when the notification was read.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
