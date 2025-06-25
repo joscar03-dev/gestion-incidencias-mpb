61546a674df4f119f241e541de5dde9d944106aa
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-06-25 13:42:49 -0500

    agregamos logica para asignar el usuario que modifica el asignado_a

--- app/Filament/Resources/TicketResource/Pages/EditTicket.php
+++ app/Filament/Resources/TicketResource/Pages/EditTicket.php
@@ -16,4 +16,19 @@ class EditTicket extends EditRecord
             // Actions\DeleteAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Obtenemos el ticket original cargado en el formulario
+        $ticket = $this->getRecord();
+
+        // Si el valor de 'asignado_a' ha cambiado
+        if ($ticket->asignado_a !== $data['asignado_a']) {
+            // Asignamos el usuario autenticado a 'asignado_por'
+            $data['asignado_por'] = auth()->id();
+        }
+
+        return $data;
+    }
+
 }
