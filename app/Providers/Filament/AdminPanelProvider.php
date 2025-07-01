018ac84ef0de1453fe967821aa3c0ef92b753532
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-07-01 12:54:13 -0500

    agragamos un widget

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\TicketResource\Widgets\TicketsTiempoResolucionChart;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -47,6 +48,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
+                TicketsTiempoResolucionChart::class,
             ])
             ->middleware([
                 EncryptCookies::class,
