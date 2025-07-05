693deda0e70ce3bf36d828b714f56234a0ac3a2f
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-07-05 08:00:25 -0500

    modificacion de widget

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Categoria;
+use App\Models\Dispositivo;
 use App\Models\Rol;
 use App\Models\Ticket;
 use App\Models\User;
@@ -12,14 +13,17 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsOverview extends BaseWidget
 {
-    // protected static ?int $sort = 1;
+    
     protected function getStats(): array
     {
         return [
 
             Stat::make('Total de tickets', Ticket::count()),
-            Stat::make('Total de categorias', Categoria::where('is_active', true)->count()),
-            Stat::make('Total de moderadores', User::role('Moderador')->count()),
+            Stat::make('Tickets Resueltos', Ticket::where('estado', Ticket::ESTADOS['Cerrado'])->count()),
+                
+            Stat::make('Total de dispositivos', Dispositivo::count())
+                ->color('success')
+                ->icon('heroicon-o-device-phone-mobile'),
         ];
     }
 }
