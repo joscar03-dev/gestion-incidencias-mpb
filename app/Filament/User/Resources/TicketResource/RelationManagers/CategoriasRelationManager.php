38e8a264cf86eabe9d845814820d0701c37383ab
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-07-07 06:15:25 -0500

    relation manager de categoria con ticket en el panel user

---
+++ app/Filament/User/Resources/TicketResource/RelationManagers/CategoriasRelationManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\User\Resources\TicketResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Actions\AttachAction;
+use Filament\Tables\Actions\DetachAction;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoriasRelationManager extends RelationManager
+{
+    protected static string $relationship = 'categorias';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nombre')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('nombre')
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre'),
+                Tables\Columns\TextColumn::make('slug'),
+                ToggleColumn::make('is_active'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+
+                AttachAction::make()
+                ->recordSelectOptionsQuery(function (Builder $query) {
+                    return $query->active();
+                })
+                ->preloadRecordSelect(),
+
+            ])
+            ->actions([
+                DetachAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
