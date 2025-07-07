f6efc0948bf38e67e7af8d7444f1efd81b1af673
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-07-07 06:05:12 -0500

    asignar al usuario un estrado de asignado si esta seleccionado el user

--- app/Filament/Resources/DispositivoResource/Pages/CreateDispositivo.php
+++ app/Filament/Resources/DispositivoResource/Pages/CreateDispositivo.php
@@ -9,4 +9,25 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateDispositivo extends CreateRecord
 {
     protected static string $resource = DispositivoResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['usuario_id'])) {
+            $data['estado'] = 'Asignado';
+        }
+        
+        return $data;
+    }
+    protected function afterCreate(): void
+    {
+        $record = $this->record;
+
+        if (!empty($record->usuario_id)) {
+            \App\Models\DispositivoAsignacion::create([
+                'dispositivo_id' => $record->id,
+                'user_id' => $record->usuario_id,
+                'fecha_asignacion' => now(),
+            ]);
+        }
+    }
 }
