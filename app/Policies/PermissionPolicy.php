d814787bbfddb1874f051e8042724d1205e58875
joscar03-dev <locomancocapac@gmail.com>
2025-07-08 20:15:17 -0500

    modificacion para acceso con permisos

--- app/Policies/PermissionPolicy.php
+++ app/Policies/PermissionPolicy.php
@@ -4,87 +4,70 @@ namespace App\Policies;
 
 use App\Models\Permission;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class PermissionPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user)
+    public function viewAny(User $user): bool
     {
-        return $user->hasRole(['Admin', 'Moderador'])
-            ? Response::allow()
-            : Response::deny('You do not have permission to view permissions.');
+        return $user->can('ver-permission');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Permission $permission)
+    public function view(User $user, Permission $permission): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to view this permission.');
+        return $user->can('ver-permission');
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user)
+    public function create(User $user): bool
     {
-        return ($user->hasRole('Admin') || $user->hasPermissionTo('crear-permiso'))
-            ? Response::allow()
-            : Response::deny('You do not have permission to create permissions.');
-
+        return $user->can('crear-permission');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Permission $permission)
+    public function update(User $user, Permission $permission): bool
     {
-        return ($user->hasRole('Admin') || $user->hasPermissionTo('editar-permiso'))
-            ? Response::allow()
-            : Response::deny('You do not have permission to update this permission.');
+        return $user->can('editar-permission');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Permission $permission)
+    public function delete(User $user, Permission $permission): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to delete this permission.');
+        return $user->can('borrar-permission');
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Permission $permission)
+    public function restore(User $user, Permission $permission): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to restore this permission.');
+        return $user->can('editar-permission');
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Permission $permission)
+    public function forceDelete(User $user, Permission $permission): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to permanently delete this permission.');
+        return $user->can('borrar-permission');
     }
+
     /**
      * Determine whether the user can perform bulk delete.
      */
-    public function bulkDelete(User $user)
+    public function bulkDelete(User $user): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to bulk delete permissions.');
+        return $user->can('borrar-permission');
     }
 }
