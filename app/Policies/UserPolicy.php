3cb611a2b014bc312e798efbbe9a3eb5f8e2a20a
joscar03-dev <locomancocapac@gmail.com>
2025-07-08 20:14:50 -0500

    modificacion para acceso con permisos

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -3,65 +3,62 @@
 namespace App\Policies;
 
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class UserPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user)
+    public function viewAny(User $user): bool
     {
-        return $user->hasRole(['Admin', 'Moderador'])
-            ? Response::allow()
-            : Response::deny('You do not have permission to view users.');
+        return $user->can('ver-user');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model)
+    public function view(User $user, User $model): bool
     {
-        return $user->hasRole('Admin');
+        return $user->can('ver-user');
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user)
+    public function create(User $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user->can('crear-user');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model)
+    public function update(User $user, User $model): bool
     {
-        return $user->hasRole('Admin');
+        return $user->can('editar-user');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, User $model)
+    public function delete(User $user, User $model): bool
     {
-        return $user->hasRole('Admin');
+        return $user->can('borrar-user');
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, User $model)
+    public function restore(User $user, User $model): bool
     {
-        return $user->hasRole('Admin');
+        return $user->can('editar-user');
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, User $model)
+    public function forceDelete(User $user, User $model): bool
     {
-        return $user->hasRole('Admin');
+        return $user->can('borrar-user');
     }
 }
