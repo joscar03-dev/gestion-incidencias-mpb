ab123045092fbdb4fcca4243e92eacde768efd44
joscar03-dev <locomancocapac@gmail.com>
2025-07-08 20:15:35 -0500

    modificacion para acceso con permisos

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -4,77 +4,62 @@ namespace App\Policies;
 
 use App\Models\Role;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class RolePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user)
+    public function viewAny(User $user): bool
     {
-        return $user->hasRole(['Admin', 'Moderador'])
-            ? Response::allow()
-            : Response::deny('You do not have permission to view roles.');
+        return $user->can('ver-role');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Role $role)
+    public function view(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to view this role.');
+        return $user->can('ver-role');
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user)
+    public function create(User $user): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to create roles.');
+        return $user->can('crear-role');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Role $role)
+    public function update(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to update this role.');
+        return $user->can('editar-role');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Role $role)
+    public function delete(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to delete this role.');
+        return $user->can('borrar-role');
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Role $role)
+    public function restore(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to restore this role.');
+        return $user->can('editar-role');
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Role $role)
+    public function forceDelete(User $user, Role $role): bool
     {
-        return $user->hasRole('Admin')
-            ? Response::allow()
-            : Response::deny('You do not have permission to permanently delete this role.');
+        return $user->can('borrar-role');
     }
 }
