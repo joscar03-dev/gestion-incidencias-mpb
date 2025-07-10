837af7f477c29c09156c83a1b0be75747486a084
joscar03-dev <locomancocapac@gmail.com>
2025-07-09 19:24:56 -0500

    argegar el comando veriuficar escalado al scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        //$schedule->command('tickets:verificar-sla')->everyFiveMinutes();
+        $schedule->command('sla:verificar-escalado')->everyFiveMinutes();
     }
 
     /**
