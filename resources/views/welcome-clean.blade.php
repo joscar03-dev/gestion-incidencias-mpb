0b5844886958d9d28564071fafd661f4976b5d21
joscar03-dev <locomancocapac@gmail.com>
2025-07-09 22:07:05 -0500

    vista clean

---
+++ resources/views/welcome-clean.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.welcome')
+
+@section('title', 'Centro de Soporte - Sistema de Gestión de Incidencias')
+
+@section('content')
+    <x-hero />
+
+    <x-quick-report />
+
+    <x-user-stats />
+
+    <x-service-categories />
+
+    <x-faq />
+
+    <x-system-status />
+@endsection
