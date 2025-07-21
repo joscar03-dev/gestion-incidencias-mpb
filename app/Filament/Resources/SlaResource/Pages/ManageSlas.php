c7ad5b20e5b9a62443b69762b2f5166658a7a68d
joscar03-dev <locomancocapac@gmail.com>
2025-07-21 18:48:50 -0500

    borrar botoon create del header

--- app/Filament/Resources/SlaResource/Pages/ManageSlas.php
+++ app/Filament/Resources/SlaResource/Pages/ManageSlas.php
@@ -13,7 +13,7 @@ class ManageSlas extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
