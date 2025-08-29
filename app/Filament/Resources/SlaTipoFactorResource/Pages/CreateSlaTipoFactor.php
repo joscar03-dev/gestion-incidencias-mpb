0e3d8fbced2f6306d151a5de745c6f7ff3fbe0a5
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-29 13:30:56 -0500

    recurso de factor sla tipo

---
+++ app/Filament/Resources/SlaTipoFactorResource/Pages/CreateSlaTipoFactor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SlaTipoFactorResource\Pages;
+
+use App\Filament\Resources\SlaTipoFactorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSlaTipoFactor extends CreateRecord
+{
+    protected static string $resource = SlaTipoFactorResource::class;
+}
