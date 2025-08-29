311487a41258e839f4a1abd49d6e058f513f6cb8
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-29 13:27:45 -0500

    recurso de factor prioridad

---
+++ app/Filament/Resources/SlaPrioridadFactorResource/Pages/EditSlaPrioridadFactor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\SlaPrioridadFactorResource\Pages;
+
+use App\Filament\Resources\SlaPrioridadFactorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditSlaPrioridadFactor extends EditRecord
+{
+    protected static string $resource = SlaPrioridadFactorResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
