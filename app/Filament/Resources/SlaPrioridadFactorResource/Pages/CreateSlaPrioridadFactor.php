8f73157dd1ca5e329595912300cea2a3b0eba1f5
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-29 13:27:01 -0500

    recurso de factor prioridad

---
+++ app/Filament/Resources/SlaPrioridadFactorResource/Pages/CreateSlaPrioridadFactor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SlaPrioridadFactorResource\Pages;
+
+use App\Filament\Resources\SlaPrioridadFactorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSlaPrioridadFactor extends CreateRecord
+{
+    protected static string $resource = SlaPrioridadFactorResource::class;
+}
