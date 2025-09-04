9ee8cc6d9e203d59fe37b94346576dd8b4e7d3bf
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-09-04 17:43:19 -0500

    0 modificaciones

--- resources/views/components/navigation/notification-dropdown.blade.php
+++ resources/views/components/navigation/notification-dropdown.blade.php
@@ -59,7 +59,7 @@
                 <div class="p-4">
                     <div class="flex items-start space-x-3">
                         <div class="flex-shrink-0">
-                            <div class="w-10 h-10 rounded-full bg-gradient-to-r from-blue-500 to-blue-600 flex items-center justify-center">
+                            <div class="w-10 h-10 rounded-full flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-600">
                                 <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"/>
                                 </svg>
