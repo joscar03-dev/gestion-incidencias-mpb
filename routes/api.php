2af1e2bd082bf14812949349cb4f4ac5a8cfe4d8
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-09-04 17:43:08 -0500

    0 modificaciones

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NotificationController;
+use App\Http\Controllers\Api\SatisfactionSurveyController;
 
 /*
 |--------------------------------------------------------------------------
