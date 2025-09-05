e8d0d3ce88a9e89db564ef3bffd5b21b538df5a3
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-09-04 19:49:12 -0500

    borramos botones

--- app/Filament/Resources/ItilDashboardResource/Pages/ListItilDashboard.php
+++ app/Filament/Resources/ItilDashboardResource/Pages/ListItilDashboard.php
@@ -12,24 +12,6 @@ class ListItilDashboard extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Action::make('view_metrics')
-                ->label('Ver Métricas Avanzadas')
-                ->icon('heroicon-m-chart-bar')
-                ->url(static::$resource::getUrl('metrics'))
-                ->color('info'),
-
-            Action::make('view_analytics')
-                ->label('Analytics ITIL')
-                ->icon('heroicon-m-chart-pie')
-                ->url(static::$resource::getUrl('analytics'))
-                ->color('success'),
-
-            Action::make('service_catalog')
-                ->label('Catálogo de Servicios')
-                ->icon('heroicon-m-squares-2x2')
-                ->url(static::$resource::getUrl('service-catalog'))
-                ->color('warning'),
-        ];
+        return [];
     }
 }
